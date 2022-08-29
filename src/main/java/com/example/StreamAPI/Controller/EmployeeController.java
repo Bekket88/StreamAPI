e4e7c6e83eca36d94e019095fd1e138ad165d7c8
Vladimir Popov <Bekket88@gmail.com>
2022-08-29 12:06:03 +0300

    Готовый проект со стримами

---
+++ src/main/java/com/example/StreamAPI/Controller/EmployeeController.java
@@ -0,0 +1,46 @@
+package com.example.StreamAPI.Controller;
+
+import com.example.StreamAPI.Model.Employee;
+import com.example.StreamAPI.Service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping(path = "/add")
+    public Employee add(@RequestParam("firstName") String firstName,
+                        @RequestParam("lastName") String lastName,
+                        @RequestParam("salary") int salary,
+                        @RequestParam("department") int department){
+        return employeeService.add(firstName, lastName, salary, department);
+    }
+
+    @GetMapping(path = "/remove")
+    public String remove(@RequestParam("firstName") String firstName,
+                           @RequestParam("lastName") String lastName){
+        return employeeService.remove(firstName, lastName);
+    }
+
+    @GetMapping(path = "/find")
+    public Employee find(@RequestParam("firstName") String firstName,
+                         @RequestParam("lastName") String lastName) {
+        return employeeService.find(firstName, lastName);
+    }
+
+    @GetMapping(path = "/print")
+    public List<Employee> print() {
+        return employeeService.printAll();
+    }
+}
---
+++ src/main/java/com/example/StreamAPI/Service/EmployeeService.java
@@ -0,0 +1,50 @@
+package com.example.StreamAPI.Service;
+
+import com.example.StreamAPI.Exception.EmployeeAlreadyAddedException;
+import com.example.StreamAPI.Exception.EmployeeNotFoundException;
+import com.example.StreamAPI.Model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class EmployeeService {
+
+    private final Map<String, Employee> employees = new HashMap<>();
+
+    private String getKey(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
+
+    public Employee add(String firstName, String lastName, int salary, int department) {
+        Employee employee = new Employee(firstName, lastName, salary, department);
+        if (employees.containsKey(getKey(firstName, lastName))) {
+            throw new EmployeeAlreadyAddedException();
+        } else {
+            employees.put(getKey(firstName, lastName), employee);
+        }
+        return employee;
+    }
+
+    public String remove(String firstName, String lastName) {
+        if (employees.containsKey(getKey(firstName, lastName))) {
+            employees.remove(getKey(firstName, lastName));
+            return "Объект успешно удален";
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    public Employee find(String firstName, String lastName) {
+        if (employees.containsKey(getKey(firstName, lastName))) {
+            return employees.get(getKey(firstName, lastName));
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    public List<Employee> printAll() {
+        return new ArrayList<>(employees.values());
+    }
+}
